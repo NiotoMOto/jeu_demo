1599059fa6efa85f2c71f5991dca7aecc69da92e
sebastienfernandez <sebastien.fdu78@gmail.com>
2018-07-22 14:40:32 +0200

    Update game.js

--- infirmerie/infirmerie/game.js
+++ infirmerie/infirmerie/game.js
@@ -1,22 +1,3 @@
-var ecran = document.getElementById("screen");
-ecran.style.backgroundImage = "url('images/lieux/exterieur.jpg')";
-ecran.style.backgroundRepeat = "no-repeat";
-/*ecran.style.backgroundAttachement = "fixed";*/
-ecran.style.backgroundPosition = "center";
-document.getElementById("texte").textContent = "Une nouvelle journée commence à l'hôpital, un nouvel hôpital au fait puisque tu es réafectée dans celui-ci suite au décès accidentel en service d'une infirmière. Bonne chance, tout devrait bien se passer...";
-
-var force = document.getElementById("force");
-
-if (force.textContent = "normale") {
-	force.style.color = "blue";
-}
-
-var sante = document.getElementById("sante");
-
-if (sante.textContent = "bonne") {
-	sante.style.color = "green";
-}
-
 var bas = document.getElementById("bas");
 var haut = document.getElementById("haut");
 var droite = document.getElementById("droite");
@@ -26,21 +7,8 @@ var texte_haut = document.getElementById("description_haut");
 var texte_droite = document.getElementById("description_droite");
 var texte_gauche = document.getElementById("description_gauche");
 var habits = document.getElementById("habits");
-var inventaire = document.getElementById("description");
-
-function clic1() {
-   ecran.style.backgroundImage = "url('images/personnages/docteur.jpg')";
-   if (habits.textContent = "propres") {
-   		document.getElementById("texte").textContent = "Bonjour Diane, je suis le docteur Derek, vous m'avez l'air en forme aujourd'hui. Je vais vous poser une question : Il y a un nouveau né dans l'aile de pédiatrie, son pouls est de 90 battements par minute, est-ce normal ? ";
-	}
-	else {
-		document.getElementById("texte").textContent = "Bonjour Diane, je suis le docteur Derek, et j'attends de mes infirmières qu'elles aient une tenue plus présentable au travail ! Laissez moi vous poser une question : Il y a un nouveau né dans l'aile de pédiatrie, son pouls est de 90 battements par minute, est-ce normal ? ";
-	}
-   texte_bas.textContent = "OUI";
-   texte_haut.textContent = "NON";
-   texte_gauche.textContent = "";
-   texte_droite.textContent = "";
-}
+habits.textContent = "propres";
+var inventaire = document.getElementById("inventaire");
 
 function clic2() {
     alert("Votre service n'est pas fini !");
@@ -48,24 +16,43 @@ function clic2() {
 
 function clic3() {
     alert("Vous vous asseyez un moment sur un banc et vous vous rendez compte qu'il y a un journal abandonné à côté, vous le prenez");
-    if (inventaire.textContent = "rien pour l'instant") {
-    	inventaire.textContent = "journal";
-	}
-	else {
-		inventaire.textContent += "journal";
-	}
+		inventaire.innerHTML += "<li>journal</li>";
 }
 
 function clic4() {
     alert("Vous vous approchez des poubelles en voulant attraper une paire de ciseaux encore utilisables quand tout d'un coup, vous glissez et salissez votre tenue de plusieurs traces de sauces fast-food...");
     habits.textContent = "sales";
-    if (inventaire.textContent = "rien pour l'instant") {
-    	inventaire.textContent = "paire de ciseaux";
-	}
-	else {
-		inventaire.textContent += "paire de ciseaux";
-	}
+	inventaire.innerHTML += "<li>paire de ciseaux</li>";
+}
 
+function clic1() {
+   ecran.style.backgroundImage = "url('images/personnages/docteur.jpg')";
+   	if (habits.textContent = "propres") {
+   		document.getElementById("texte").textContent = "Bonjour Diane, je suis le docteur Derek, vous m'avez l'air en forme aujourd'hui. Je vais vous poser une question : Il y a un nouveau né dans l'aile de pédiatrie, son pouls est de 90 battements par minute, est-ce normal ? ";
+	}
+	if (habits.textContent = "sales") {
+		document.getElementById("texte").textContent = "Bonjour Diane, je suis le docteur Derek, et j'attends de mes infirmières qu'elles aient une tenue plus présentable au travail ! Laissez moi vous poser une question : Il y a un nouveau né dans l'aile de pédiatrie, son pouls est de 90 battements par minute, est-ce normal ? ";
+	}
+	
+   texte_bas.textContent = "OUI";
+   bas.id = "question1_oui";
+   texte_haut.textContent = "NON";
+   haut.id = "question1_non";
+   texte_gauche.textContent = "";
+   texte_droite.textContent = "";
+}
+
+var reponse1 = document.getElementById("question1_oui");
+var reponse2 = document.getElementById("question1_non");
+
+function clic5() {
+	document.getElementById("texte").textContent = "Je vois, vous laisseriez donc ce nouveau-né avec une insuffisance cardiaque manifeste, j'en toucherai un mot à la directrice";
+	alert("La démo est finie, le jeu sera complété plus tard");
+}
+
+function clic6() {
+	document.getElementById("texte").textContent = "Bonne réponse Diane, je suis ravi d'avoir une infirmière compétente avec moi, je vous souhaite une bonne journée";
+	alert("La démo est finie, le jeu sera complété plus tard");
 }
 
 
@@ -73,3 +60,5 @@ haut.addEventListener("click", clic1);
 bas.addEventListener("click", clic2);
 droite.addEventListener("click", clic3);
 gauche.addEventListener("click", clic4);
+reponse1.addEventListener("click", clic5);
+reponse2.addEventListener("click", clic6);
